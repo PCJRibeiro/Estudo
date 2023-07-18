7d764104b96ac97a77468838959eaa3c73583654
PCJRibeiro <pcjr2003@gmail.com>
2023-07-18 12:05:07 -0300

    Estudo com string build e enum em Contrato de trabalho

---
+++ ContratoDeTrabalho/ContratoDeTrabalho/Entites/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ContratoDeTrabalho.Entites.Enums
+{
+     enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+
+}
---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Postagem.Entites;
+
+namespace Postagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1= new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2023 13:05:44"),"Traveling to New Zeland","to indo pra um lugar longe pra carai",12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Vai toma no cu piranha");
+            Comment c4 = new Comment("Devendo 5k pro agiota e ta viajando");
+            Post p2 = new Post(
+                DateTime.Parse("12/07/2023 14:23:20"), "thank you", "Tyler banger",90);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
